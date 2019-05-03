6e6a4847eb4edcc10d1f1efe62d8d4fc6b9316ac
Gode King <GengGode@outlook.com>
2019-05-03 23:54:35 +0800

    05测试 稍作修改，未多屏测试

--- 05多屏全屏/Form1.Designer.cs
+++ 05多屏全屏/Form1.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -41,23 +43,45 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(156, 203);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(58, 232);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(300, 21);
+            this.textBox1.TabIndex = 2;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(370, 395);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- 05多屏全屏/Form1.cs
+++ 05多屏全屏/Form1.cs
@@ -21,13 +21,46 @@ namespace _05多屏全屏
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //this.WindowState = FormWindowState.Maximized;
-            int i = 0;
-            this.DesktopBounds = new Rectangle(Screen.AllScreens[i].WorkingArea.X, Screen.AllScreens[i].WorkingArea.Y, Screen.AllScreens[i].WorkingArea.Width, Screen.AllScreens[i].WorkingArea.Height);
-            this.Location = new Point(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y);
-
+            
             SetBackGround();
+            SetFullScreen(comboBox1.SelectedIndex);
+            textBox1.Text = this.DesktopBounds.ToString() + this.Location.ToString();
+            textBox1.Text = textBox1.Text;
         }
+
+        #region 窗口全屏（SetFullScreen(int i)）
+        public void SetFullScreen(int i)
+        {
+            //this.DesktopLocation = new Point(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y);
+            //this.DesktopLocation = new Point(this.Bounds.X, this.Bounds.Y);
+            
+            this.DesktopBounds = new Rectangle(Screen.AllScreens[i].WorkingArea.X, Screen.AllScreens[i].WorkingArea.Y, Screen.AllScreens[i].WorkingArea.Width, Screen.AllScreens[i].WorkingArea.Height);
+            this.Location = new Point(Screen.AllScreens[i].Bounds.X + Screen.AllScreens[i].WorkingArea.X, Screen.AllScreens[i].Bounds.Y + Screen.AllScreens[i].WorkingArea.Y);
+            
+            //this.ClientSize.Width = Screen.AllScreens[i].Bounds.Width;       
+            //this.ClientSize.Height = Screen.AllScreens[i].Bounds.Height;
+            /*
+             
+             */
+            /*
+            if (System.Windows.Forms.Screen.AllScreens.Count() != 1)
+            {
+                if (System.Windows.Forms.Screen.AllScreens.Count() > i)
+                {
+                    this.Location = new Point(Screen.AllScreens[i].Bounds.X, Screen.AllScreens[i].Bounds.Y);
+                    this.Width = Screen.AllScreens[i].Bounds.Width;
+                    this.Height = Screen.AllScreens[i].Bounds.Height;
+                }
+            }
+            else
+            {
+                this.Location = new Point(Screen.AllScreens[0].Bounds.X, Screen.AllScreens[0].Bounds.Y);
+                this.Width = Screen.AllScreens[0].Bounds.Width;
+                this.Height = Screen.AllScreens[0].Bounds.Height;
+            }
+            */
+        }
+        #endregion
 
         #region 设置为桌面（SetBackGround()）
         private void SetBackGround()
@@ -50,5 +83,17 @@ namespace _05多屏全屏
             Win32.SetParent(this.Handle, Ptr);
         }
         #endregion
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int Count = Screen.AllScreens.Count();
+            int i;
+            for (i = 0; i < Count; i++)
+            {
+                String screen = Screen.AllScreens[i].DeviceName;//.ToString();
+                comboBox1.Items.Add(screen);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
     }
 }
